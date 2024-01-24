bedace400a7a237c4052a7a4ec02d16a65e03b26
Gleb Kuimov <kuimov.gleb30122002@gmail.com>
2024-01-24 21:40:12 +0300

    создал контроллер Product

--- avito/Controllers/ProductController.cs
+++ avito/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using avito.Interfaces;
+using avito.Models;
+using avito.Repository;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace avito.Controllers
@@ -7,5 +10,22 @@ namespace avito.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly IProductRepository _productRepository;
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        [HttpGet("{id}")]
+        [ProducesResponseType(200, Type = typeof(Product))]
+        [ProducesResponseType(400)]
+        public IActionResult GetProduct(int id)
+        {
+            if (_productRepository.ProductExists(id))
+            {
+                return NotFound();
+            }
+            var product = _productRepository.GetProduct(id);
+            return Ok(product);
+        }
     }
 }
