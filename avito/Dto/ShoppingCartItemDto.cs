f8109da8067e2b66e6d0c5cc268f32af6bf943e4
Glebas34 <147874028+Glebas34@users.noreply.github.com>
2024-04-11 19:33:23 +0300

    Delete directory

--- avito/Dto/ShoppingCartItemDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace avito.Dto
-{
-    public class ShoppingCartItemDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Price { get; set; }
-    }
-}
